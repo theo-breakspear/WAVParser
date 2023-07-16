fc9ca96cc24284a3c22ea79821a6492fb8f40cbc
theo-breakspear <136743256+theo-breakspear@users.noreply.github.com>
2023-07-16 16:14:51 +0100

    implement drag (but not drop)

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,26 @@
-console.log('hello world')
+function initialiseFileDrop() {
+	const dropZone = document.getElementById('drop-zone')
+	if (!dropZone) {
+		throw new Error('Could not find dropzone element')
+	}
+
+	dropZone.addEventListener('dragover', (event) => {
+		event.preventDefault()
+	})
+
+	dropZone.addEventListener('dragenter', (event) => {
+		event.preventDefault()
+		dropZone.classList.add('file-over')
+		dropZone.innerText = 'drop here'
+	})
+
+	dropZone.addEventListener('dragleave', (event) => {
+		event.preventDefault()
+		dropZone.classList.remove('file-over')
+		dropZone.innerText = 'Drag File'
+	})
+}
+
+window.addEventListener('load', () => {
+	initialiseFileDrop()
+})
