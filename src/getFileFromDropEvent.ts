654a37a701f7b2a15f0a04dc22f140fefa8e3aef
theo-breakspear <136743256+theo-breakspear@users.noreply.github.com>
2023-07-18 11:11:44 +0100

    update check file exists in getFileFromDropEvent

--- src/getFileFromDropEvent.ts
+++ src/getFileFromDropEvent.ts
@@ -1,7 +1,7 @@
 export function getFileFromDropEvent(event: DragEvent) {
-	if (event.dataTransfer?.files[0]) {
+	if (event.dataTransfer?.files) {
 		return event.dataTransfer.files[0]
 	} else {
-		throw new Error('No file found')
+		throw new Error('No file dropped')
 	}
 }
