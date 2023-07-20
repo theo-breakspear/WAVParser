cc4d18e1ec11d91f5637d238e00a7c2afb3adca8
theo-breakspear <136743256+theo-breakspear@users.noreply.github.com>
2023-07-20 11:10:21 +0100

    use factory class to better deal with async constructor

--- src/app/classes/WavHeader.ts
+++ src/app/classes/WavHeader.ts
@@ -1,44 +1,17 @@
-type HeaderField = string | number | undefined
+type HeaderField = string | number
 
 export class WavHeader {
-	ChunkID: HeaderField
-	ChunkSize: HeaderField
-	Format: HeaderField
-	Subchunk1ID: HeaderField
-	Subchunk1Size: HeaderField
-	AudioFormat: HeaderField
-	NumChannels: HeaderField
-	SampleRate: HeaderField
-	ByteRate: HeaderField
-	BlockAlign: HeaderField
-	BitsPerSample: HeaderField
-
-	async populate(wavFile: File) {
-		this.ChunkID = await this.setHeaderField(0, 4, true, wavFile)
-		this.ChunkSize = await this.setHeaderField(4, 4, false, wavFile)
-		this.Format = await this.setHeaderField(8, 4, true, wavFile)
-		this.Subchunk1ID = await this.setHeaderField(12, 4, true, wavFile)
-		this.Subchunk1Size = await this.setHeaderField(16, 4, false, wavFile)
-		this.AudioFormat = await this.setHeaderField(20, 2, false, wavFile)
-		this.NumChannels = await this.setHeaderField(22, 2, false, wavFile)
-		this.SampleRate = await this.setHeaderField(24, 4, false, wavFile)
-		this.ByteRate = await this.setHeaderField(28, 4, false, wavFile)
-		this.BlockAlign = await this.setHeaderField(32, 2, false, wavFile)
-		this.BitsPerSample = await this.setHeaderField(34, 2, false, wavFile)
-	}
-
-	private async setHeaderField(
-		offset: number,
-		size: number,
-		parseAsText: boolean,
-		wavFile: File
-	): Promise<string | number> {
-		const parseText = async (blob: Blob) => await blob.text()
-		const parseInt = async (blob: Blob) =>
-			new Uint8Array(await blob.arrayBuffer()).reduce(
-				(acc, cur, i) => acc + (cur << (i * 8))
-			)
-		const blob = wavFile.slice(offset, offset + size)
-		return await (parseAsText ? parseText(blob) : parseInt(blob))
-	}
+	constructor(
+		public chunkID: HeaderField,
+		public chunkSize: HeaderField,
+		public format: HeaderField,
+		public subchunk1ID: HeaderField,
+		public subchunk1Size: HeaderField,
+		public audioFormat: HeaderField,
+		public numChannels: HeaderField,
+		public sampleRate: HeaderField,
+		public byteRate: HeaderField,
+		public blockAlign: HeaderField,
+		public bitsPerSample: HeaderField
+	) {}
 }
---
+++ src/app/classes/WavHeaderFactory.ts
@@ -0,0 +1,48 @@
+import { WavHeader } from './WavHeader'
+
+export class WavHeaderFactory {
+	constructor() {}
+
+	async create(wavFile: File): Promise<WavHeader> {
+		const chunkID = await this.setHeaderField(0, 4, true, wavFile)
+		const chunkSize = await this.setHeaderField(4, 4, false, wavFile)
+		const format = await this.setHeaderField(8, 4, true, wavFile)
+		const subchunk1ID = await this.setHeaderField(12, 4, true, wavFile)
+		const subchunk1Size = await this.setHeaderField(16, 4, false, wavFile)
+		const audioFormat = await this.setHeaderField(20, 2, false, wavFile)
+		const numChannels = await this.setHeaderField(22, 2, false, wavFile)
+		const sampleRate = await this.setHeaderField(24, 4, false, wavFile)
+		const byteRate = await this.setHeaderField(28, 4, false, wavFile)
+		const blockAlign = await this.setHeaderField(32, 2, false, wavFile)
+		const bitsPerSample = await this.setHeaderField(34, 2, false, wavFile)
+
+		return new WavHeader(
+			chunkID,
+			chunkSize,
+			format,
+			subchunk1ID,
+			subchunk1Size,
+			audioFormat,
+			numChannels,
+			sampleRate,
+			byteRate,
+			blockAlign,
+			bitsPerSample
+		)
+	}
+
+	private async setHeaderField(
+		offset: number,
+		size: number,
+		parseAsText: boolean,
+		wavFile: File
+	): Promise<string | number> {
+		const parseText = async (blob: Blob) => await blob.text()
+		const parseInt = async (blob: Blob) =>
+			new Uint8Array(await blob.arrayBuffer()).reduce(
+				(acc, cur, i) => acc + (cur << (i * 8))
+			)
+		const blob = wavFile.slice(offset, offset + size)
+		return await (parseAsText ? parseText(blob) : parseInt(blob))
+	}
+}
